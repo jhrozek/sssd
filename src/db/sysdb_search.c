f8dae171cffe904df0695cb0638511adb8c5c3c6
Lukas Slebodnik <lslebodn@redhat.com>
2013-11-12 14:39:27 +0100

    SYSDB: Skip malformed netgroup attribute.
    It was not easy find out why netgroup could not be covert into result entries.
    Problem was that nisNetgroupTriple contained unexpected string "(,user01)"
    This patch will ignore only malformed attribute and processing of netgroup
    will not fail.
    
    Resolves:
    https://fedorahosted.org/sssd/ticket/2137

--- src/db/sysdb_search.c
+++ src/db/sysdb_search.c
@@ -738,7 +738,11 @@ errno_t sysdb_netgr_to_entries(TALLOC_CTX *mem_ctx,
                                         &tmp_entry[c]->value.triple.username,
                                         &tmp_entry[c]->value.triple.domainname);
                     if (ret != EOK) {
-                        goto done;
+                        DEBUG(1,
+                              ("Cannot split netgroup triple [%s], "
+                               "this attribute will be skipped \n",
+                               triple_str));
+                        continue;
                     }
 
                     c++;
