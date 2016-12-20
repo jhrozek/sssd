bc50e85d2c45bd45eb942f10b0e21848fc70897c
Nikolai Kondrashov <Nikolai.Kondrashov@redhat.com>
2016-12-20 10:16:47 +0200

    CONFIG: Add session_recording section
    Add information on "session_recording" config section, having three
    options: "scope", "users", and "groups".
    
    The section is intended for disabling session recording ("scope = none",
    default), enabling session recording for all users ("scope = all"), and
    enabling it for some specific users and/or groups ("scope = some",
    "users = <users>", "groups = <groups>").

--- src/config/SSSDConfigTest.py
+++ src/config/SSSDConfigTest.py
@@ -1363,7 +1363,8 @@ class SSSDConfigTestSSSDConfig(unittest.TestCase):
             'ssh',
             'pac',
             'ifp',
-            'secrets']
+            'secrets',
+            'session_recording']
         for section in control_list:
             self.assertTrue(sssdconfig.has_section(section),
                             "Section [%s] missing" %
@@ -1457,7 +1458,8 @@ class SSSDConfigTestSSSDConfig(unittest.TestCase):
             'ssh',
             'pac',
             'ifp',
-            'secrets']
+            'secrets',
+            'session_recording']
         service_list = sssdconfig.list_services()
         for service in control_list:
             self.assertTrue(service in service_list,
